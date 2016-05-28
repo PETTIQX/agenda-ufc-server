032987afe9d042006137e9a642d6fd7883ca8c14
Alexsandro <sandrooliveira1501@gmail.com>
2016-05-28 11:41:22 -0300

    Criação de rotas públicas e privadas

--- routes/public/index.js
+++ routes/public/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var router = express.Router()
 
-router.use('/private', require('./private'))
-router.use('/public', require('./public'))
+//router.use('/private', require('./private'))
 
 module.exports = router
