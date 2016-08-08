fc709adccfad3c480e75ee9674bc50fe9a841141
sandrooliveira1501 <sandrooliveira1501@gmail.com>
2016-08-08 10:41:54 -0300

    Correção arquivo CORS

--- cors.js
+++ cors.js
@@ -1,8 +1,30 @@
 module.exports = function() {
     return function(req, res, next) {
-        console.log("CORS enable")
-        res.header("Access-Control-Allow-Origin", "*")
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next()
+        //console.log("CORS enable")
+        //console.log(req.headers)
+        var oneof = false;
+        if(req.headers.origin) {
+            res.header('Access-Control-Allow-Origin', req.headers.origin);
+            oneof = true;
+        }
+        if(req.headers['access-control-request-method']) {
+            res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+            oneof = true;
+        }
+        if(req.headers['access-control-request-headers']) {
+            res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+            oneof = true;
+        }
+        if(oneof) {
+            res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+        }
+
+        // intercept OPTIONS method
+        if (oneof && req.method == 'OPTIONS') {
+            res.send(200);
+        }
+        else {
+            next();
+        }
     }
 }
